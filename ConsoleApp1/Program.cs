c513c6f3cedd6d46c1d41b80771fd24f0bcf4bd7
doonee <d2607@naver.com>
2018-11-11 21:48:06 +0900

    초기 커밋.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection1 = new ArrayList() { 1, 2, 3 };
+            var collection2 = new ArrayList() { 1, 2, 4, 5 };
+            var mergedList = new ArrayList();
+            mergedList.AddRange(collection1);
+            mergedList.AddRange(collection2.ToArray().Where(item => !collection1.ToArray().Contains(item)).ToList());
+            Console.WriteLine(String.Join(", ", mergedList.ToArray()));
+            Console.ReadLine();
+        }
+    }
+}
